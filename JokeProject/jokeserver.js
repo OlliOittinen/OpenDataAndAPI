d9445422cdbe0add73ac39131ad0dc1c54465ac9
Olli Oittinen <ollioi@metropolia.fi>
2018-09-27 14:27:21 +0300

    work on node.js project, started working on Joke API project

---
+++ JokeProject/jokeserver.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+/*
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "jokes"
+});
+*/
+
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port)
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello world!');
+});
