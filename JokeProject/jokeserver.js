c4da4056a0ea42721e5cbced18f2453afa08a349
Olli Oittinen <ollioi@metropolia.fi>
2018-09-27 15:46:36 +0300

    started work on GET for jokes

--- JokeProject/jokeserver.js
+++ JokeProject/jokeserver.js
@@ -2,14 +2,13 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 
-/*
 const con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
     database: "jokes"
 });
-*/
+
 app.get('/', function (req, res) {
     res.sendFile("home.html", {root: __dirname});
 });
@@ -19,4 +18,15 @@ var server = app.listen(8081, function () {
     var port = server.address().port;
 
     console.log("Example app listening at http://%s:%s", host, port)
+});
+
+app.get('/getBarJokes', function (req, res) {
+
+    var sql = "SELECT * FROM jokes;";
+
+    con.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+    });
 });
