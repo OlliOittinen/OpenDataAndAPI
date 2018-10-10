9fff9b5d988367de99427fc22cfdc14632a216f7
Olli Oittinen <ollioi@metropolia.fi>
2018-10-10 12:43:55 +0300

    actual rest api style xd

--- JokeProject/jokeserver.js
+++ JokeProject/jokeserver.js
@@ -19,6 +19,8 @@ admin.initializeApp({
     databaseURL: "https://joke-api-578e4.firebaseio.com/"
 });
 
+var fbref = firebase.database().ref('/categories/');
+
 app.get('/', function (req, res) {
     res.sendFile("home.html", {root: __dirname});
 });
@@ -31,12 +33,60 @@ var server = app.listen(8081, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("app listening at http://%s:%s", host, port)
 });
 
-app.get('/Bar', function (req, res) {
-    var fbref = firebase.database().ref('/categories/Bar');
-    fbref.on("value", function (snapshot) {
-        res.send(snapshot.val()); //suoraan JSON muotoinen kakka
+function snapshotToArray(snapshot) {
+    var returnArr = [];
+
+    snapshot.forEach(function (childSnapshot) {
+        //hae json objektin tiedot, tallenna muuttujaan
+        var item = childSnapshot.val();
+        //tämän objektin avain saa saman arvon kuin objektin avain
+        item.key = childSnapshot.key;
+        returnArr.push(item);
+    });
+    return returnArr;
+};
+
+app.get('/Jokes', async function (req, res) {
+    let category = req.query.category;
+    fbref = firebase.database().ref('/categories/' + category);
+    fbref.once("value", function (snapshot) {
+        var array = snapshotToArray(snapshot);
+        res.send(array);
     })
 });
+
+app.get('/Submit', function (req, res) {
+    let category = req.query.category;
+    let joke = req.query.joke;
+    firebase.database().ref('categories/' + category).push({
+        joke: joke,
+        likes: 0
+    });
+    res.send("Joke submitted!");
+});
+
+app.get('/Vote', function (req, res) {
+
+    let jokeKey = req.query.jokeKey;
+    let integer = parseInt(req.query.integer);
+
+    var fbref = firebase.database().ref('/categories/');
+    var currentVotes = 0;
+
+    fbref.orderByKey().on("child_added", function (snapshot) {
+        snapshot.forEach(function (data) {
+            if (jokeKey === data.key) {
+                currentVotes = data.child("likes").val();
+                fbref = firebase.database().ref('/categories/' + snapshot.key + '/' + data.key);
+                var newValue = currentVotes + integer;
+                fbref.update({
+                    likes: newValue
+                });
+            }
+        })
+    });
+    res.send();
+})
